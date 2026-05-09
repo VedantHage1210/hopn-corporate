e1bc5eff3631f7b1b9d49ead9a863701d7949bc2
Vedant Hage <vedanthage4@gmail.com>
2026-05-09 10:25:18 +0530

    Add Arabic language support in SetLocale middleware

--- app/Http/Middleware/SetLocale.php
+++ app/Http/Middleware/SetLocale.php
@@ -1,31 +1,20 @@
 <?php
-
 namespace App\Http\Middleware;
-
 use Closure;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class SetLocale
 {
-    /**
-     * Handle an incoming request.
-     *
-     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
-     */
     public function handle(Request $request, Closure $next): Response
     {
         $locale = $request->route('lang');
-
-        if (! in_array($locale, ['en', 'de'], true)) {
-            $preferred = $request->getPreferredLanguage(['en', 'de']) ?? 'en';
+        if (! in_array($locale, ['en', 'de', 'ar'], true)) {
+            $preferred = $request->getPreferredLanguage(['en', 'de', 'ar']) ?? 'en';
             app()->setLocale($preferred);
-
             return $next($request);
         }
-
         app()->setLocale($locale);
-
         return $next($request);
     }
 }
