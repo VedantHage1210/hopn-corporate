cbe071e0f5caf1db85c3045d6f5b2d453e48eaf7
Vedant Hage <vedanthage4@gmail.com>
2026-05-16 20:39:54 +0530

    Add team members to about page view

--- app/Http/Controllers/Public/PageController.php
+++ app/Http/Controllers/Public/PageController.php
@@ -1,16 +1,15 @@
 <?php
-
 namespace App\Http\Controllers\Public;
-
 use App\Http\Controllers\Controller;
 use App\Models\Page;
+use App\Models\TeamMember;
 
 class PageController extends Controller
 {
     public function about()
     {
         $page = Page::query()->where('slug', 'about')->first();
-
-        return view('public.pages.about', compact('page'));
+        $teamMembers = TeamMember::orderBy('sort_order')->get();
+        return view('public.pages.about', compact('page', 'teamMembers'));
     }
 }
