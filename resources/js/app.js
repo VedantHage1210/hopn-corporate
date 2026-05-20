63b744a4b9a68644d72264f27aba8566fc7b42f2
Vedant Hage <vedanthage4@gmail.com>
2026-05-21 00:53:37 +0530

    Implement Book a Call modal functions
    Added functions to open and close the Book a Call modal.

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,24 @@
 import './bootstrap';
-
 import Alpine from 'alpinejs';
-
 window.Alpine = Alpine;
+
+// Book a Call Modal functions - defined before Alpine starts
+window.openBookCall = function() {
+    var modal = document.getElementById('book-call-modal');
+    if (modal) {
+        modal.style.display = 'flex';
+        modal.style.alignItems = 'center';
+        modal.style.justifyContent = 'center';
+        document.body.style.overflow = 'hidden';
+    }
+};
+
+window.closeBookCall = function() {
+    var modal = document.getElementById('book-call-modal');
+    if (modal) {
+        modal.style.display = 'none';
+        document.body.style.overflow = '';
+    }
+};
 
 Alpine.start();
