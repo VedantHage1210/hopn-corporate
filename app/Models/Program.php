a4883fd295faf85274a3bdceaf15f5952fc9a25e
Vedant Hage <vedanthage4@gmail.com>
2026-05-24 13:21:12 +0530

    Update Program.php

--- app/Models/Program.php
+++ app/Models/Program.php
@@ -1,7 +1,5 @@
 <?php
-
 namespace App\Models;
-
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Spatie\Activitylog\Traits\LogsActivity;
 use Spatie\Activitylog\LogOptions;
@@ -15,12 +13,17 @@ class Program extends Model
     protected $fillable = [
         'title_en',
         'title_de',
+        'title_ar',
         'slug',
         'summary_en',
         'summary_de',
+        'summary_ar',
         'audience_en',
         'audience_de',
+        'audience_ar',
+        'duration',
         'duration_weeks',
+        'image_url',
         'outcomes',
         'pricing_tiers',
         'cta_label_en',
@@ -31,16 +34,17 @@ class Program extends Model
     ];
 
     protected $casts = [
-        'outcomes' => 'array',
+        'outcomes'      => 'array',
         'pricing_tiers' => 'array',
-        'is_published' => 'boolean',
-        'published_at' => 'datetime',
+        'is_published'  => 'boolean',
+        'published_at'  => 'datetime',
     ];
 
     public function modules()
     {
         return $this->hasMany(ProgramModule::class)->orderBy('sort_order');
     }
+
     public function getActivitylogOptions(): LogOptions
     {
         return LogOptions::defaults()->logAll();
