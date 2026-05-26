ef09fe8476b1f8b784f2451719bdb39d9451d582
Vedant Hage <vedanthage4@gmail.com>
2026-05-26 09:30:39 +0530

    Update CaseStudy.php

--- app/Models/CaseStudy.php
+++ app/Models/CaseStudy.php
@@ -1,7 +1,5 @@
 <?php
-
 namespace App\Models;
-
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Spatie\Activitylog\Traits\LogsActivity;
 use Spatie\Activitylog\LogOptions;
@@ -13,34 +11,28 @@ class CaseStudy extends Model
     use HasFactory, SoftDeletes, LogsActivity;
 
     protected $fillable = [
-        'title_en', 'title_de', 'title_ar', 
+        'title_en', 'title_de', 'title_ar',
         'client_name_en', 'client_name_de', 'client_name_ar',
-        'slug', 'industry', 
+        'slug', 'industry',
         'challenge_en', 'challenge_de', 'challenge_ar',
         'solution_en', 'solution_de', 'solution_ar',
         'outcomes_en', 'outcomes_de', 'outcomes_ar',
-        'tech_stack', 
-        'image_url',   
-        'pdf_url',     
-        'is_published', 
+        'tech_stack',
+        'image_url',
+        'pdf_url',
+        'is_published',
         'published_at',
+        'industry_ids',
+        'service_ids',
     ];
 
     protected $casts = [
-        'tech_stack' => 'array',
+        'tech_stack'   => 'array',
+        'industry_ids' => 'array',
+        'service_ids'  => 'array',
         'is_published' => 'boolean',
         'published_at' => 'datetime',
     ];
-
-    public function industries()
-    {
-        return $this->belongsToMany(Industry::class);
-    }
-
-    public function services()
-    {
-        return $this->belongsToMany(Service::class);
-    }
 
     public function getActivitylogOptions(): LogOptions
     {
