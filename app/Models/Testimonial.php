aadf7593af76636409afe6a2a4b3fdee2c6873eb
Vedant Hage <vedanthage4@gmail.com>
2026-05-28 10:40:31 +0530

    Update Testimonial.php

--- app/Models/Testimonial.php
+++ app/Models/Testimonial.php
@@ -1,7 +1,5 @@
 <?php
-
 namespace App\Models;
-
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Spatie\Activitylog\Traits\LogsActivity;
 use Spatie\Activitylog\LogOptions;
@@ -13,19 +11,16 @@ class Testimonial extends Model
     use HasFactory, SoftDeletes, LogsActivity;
 
     protected $fillable = [
-        'quote_en',
-        'quote_de',
-        'author_name',
-        'author_role',
-        'company',
-        'avatar',
-        'sort_order',
-        'visible',
+        'quote_en', 'quote_de', 'quote_ar',
+        'author_name', 'author_role',
+        'company', 'avatar',
+        'sort_order', 'visible',
     ];
 
     protected $casts = [
         'visible' => 'boolean',
     ];
+
     public function getActivitylogOptions(): LogOptions
     {
         return LogOptions::defaults()->logAll();
