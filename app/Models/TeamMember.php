baf5afcae46b0936b687ad6d4bd60e5e5b39aa34
Vedant Hage <vedanthage4@gmail.com>
2026-05-30 18:44:29 +0530

    Update TeamMember.php

--- app/Models/TeamMember.php
+++ app/Models/TeamMember.php
@@ -1,12 +1,10 @@
 <?php
-
 namespace App\Models;
-
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Spatie\Activitylog\Traits\LogsActivity;
-use Spatie\Activitylog\LogOptions;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Spatie\Activitylog\Traits\LogsActivity;
+use Spatie\Activitylog\LogOptions;
 
 class TeamMember extends Model
 {
@@ -14,10 +12,8 @@ class TeamMember extends Model
 
     protected $fillable = [
         'name',
-        'role_en',
-        'role_de',
-        'bio_en',
-        'bio_de',
+        'role_en', 'role_de', 'role_ar',
+        'bio_en',  'bio_de',  'bio_ar',
         'photo',
         'linkedin',
         'sort_order',
@@ -27,6 +23,7 @@ class TeamMember extends Model
     protected $casts = [
         'visible' => 'boolean',
     ];
+
     public function getActivitylogOptions(): LogOptions
     {
         return LogOptions::defaults()->logAll();
