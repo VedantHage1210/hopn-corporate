1d1e7e41fa5cfa0e2256aae39c89d801078411cc
Vedant Hage <vedanthage4@gmail.com>
2026-05-31 18:59:37 +0530

    Update NavigationItem.php

--- app/Models/NavigationItem.php
+++ app/Models/NavigationItem.php
@@ -1,29 +1,15 @@
-<?php
+protected $fillable = [
+    'parent_id',
+    'menu_location',
+    'sort_order',
+    'label_en', 'label_de', 'label_ar',
+    'url',
+    'page_id',
+    'visible_en', 'visible_de', 'visible_ar',
+];
 
-namespace App\Models;
-
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\SoftDeletes;
-
-class NavigationItem extends Model
-{
-    use HasFactory, SoftDeletes;
-
-    protected $fillable = [
-        'parent_id',
-        'menu_location',
-        'sort_order',
-        'label_en',
-        'label_de',
-        'url',
-        'page_id',
-        'visible_en',
-        'visible_de',
-    ];
-
-    protected $casts = [
-        'visible_en' => 'boolean',
-        'visible_de' => 'boolean',
-    ];
-}
+protected $casts = [
+    'visible_en' => 'boolean',
+    'visible_de' => 'boolean',
+    'visible_ar' => 'boolean',
+];
