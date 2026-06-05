b25096d0ab42e80aff84f7e05a0e862f9ca158db
Vedant Hage <vedanthage4@gmail.com>
2026-06-05 19:17:57 +0530

    Update StartupController.php

--- app/Http/Controllers/Public/StartupController.php
+++ app/Http/Controllers/Public/StartupController.php
@@ -2,13 +2,11 @@
 namespace App\Http\Controllers\Public;
 use App\Http\Controllers\Controller;
 use App\Models\Startup;
-
 class StartupController extends Controller
 {
     public function index()
     {
-        $lang     = request()->route('lang', 'en');
         $startups = Startup::latest()->get();
-        return view('public.startups.index', compact('startups', 'lang'));
+        return view('public.startups.index', compact('startups'));
     }
 }
