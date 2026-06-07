b8a8199206253266c669e73100c945edad633ae5
Vedant Hage <vedanthage4@gmail.com>
2026-06-07 21:29:17 +0530

    Update Startup.php

--- app/Models/Startup.php
+++ app/Models/Startup.php
@@ -2,14 +2,26 @@
 namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+
 class Startup extends Model
 {
     use HasFactory;
+
     protected $fillable = [
         'name',
+        'logo',
         'industry',
+        'industry_de',
+        'industry_ar',
         'stage',
         'website',
         'description',
+        'description_de',
+        'description_ar',
+        'is_visible',
+    ];
+
+    protected $casts = [
+        'is_visible' => 'boolean',
     ];
 }
